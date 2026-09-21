a429e63b766aa8ff6bfd17e2169ed9f14d24f5f6
Pzsombor17 <Pap.Zsombor@C19-01>
2026-09-21 14:12:04 +0200

    ii.rész

---
+++ 1_het/VendingMachine/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public class Product
+    {
+        private string _code;
+        private string _name;
+        private int _price;
+        private int _stock;
+        private ProductCategory _category;
+
+        public string Code { get {return _code; } }
+        public string Name { get { return _name; } }
+        public int Price { get { return _price; } }
+        public int Stock { get { return _stock; } }
+        public ProductCategory Category { get { return _category; } }
+        public bool IsAviable { get { if (_stock > 0) { return true; } else { return false; } } }
+
+        public Product(string Code,string Name,int Price, int Stock, ProductCategory Category)
+        {
+            _code = Code;
+            _name = Name;
+            _price = Price;
+            _stock = Stock;
+            _category = Category;
+        }
+        public void Sell()
+        {
+            _stock--;
+        }
+    }
+}
---
+++ 1_het/VendingMachine/ProductCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    
+        public enum ProductCategory
+        {
+            Drink,Snack,Food
+        }
+        
+    
+}
