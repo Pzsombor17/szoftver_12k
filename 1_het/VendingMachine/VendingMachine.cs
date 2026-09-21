0e4c8ae92dee677127209d64d8ce8a3fa3b4867e
Pzsombor17 <Pap.Zsombor@C19-01>
2026-09-21 14:30:41 +0200

    haladtam  13.feladat

---
+++ 1_het/VendingMachine/VendingMachine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public class VendingMachine
+    {
+        private List<Product> _products;
+        private int _balance;
+        private int _income;
+
+        public int Balance { get; }
+        public int Income { get; }
+
+        private ProductCategory ParseCategory(string parameter)
+        {
+            if (parameter == "0")
+            {
+                return ProductCategory.Drink;
+            }
+        }
+    }
+}
